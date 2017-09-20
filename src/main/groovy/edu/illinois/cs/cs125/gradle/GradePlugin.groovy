6bdc480f1b3f3320089f6a6bdff454c4d2c226d7
Geoffrey Challen <challen@illinois.edu>
2017-09-20 14:58:15 -0500

    Push to try jitpack.

--- src/main/groovy/edu/illinois/cs/cs125/gradle/GradePlugin.groovy
+++ src/main/groovy/edu/illinois/cs/cs125/gradle/GradePlugin.groovy
@@ -3,10 +3,11 @@ package edu.illinois.cs.cs125.gradle
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 
+/**
+ * Plugin containing our grade task.
+ */
 class GradePlugin implements Plugin<Project> {
     void apply(Project target) {
-		target.task('grade', type: GradeTask)
+        target.task('grade', type: GradeTask)
     }
 }
-
-// vim: ts=4:sw=4:et:ft=groovy
--- src/main/groovy/edu/illinois/cs/cs125/gradle/GradeTask.groovy
+++ src/main/groovy/edu/illinois/cs/cs125/gradle/GradeTask.groovy
@@ -18,143 +18,167 @@ import org.apache.http.client.methods.*
 import org.apache.http.entity.*
 import org.apache.http.impl.client.*
 
+/**
+ * Class implementing our Gradle grade task.
+ */
 class GradeTask extends DefaultTask {
 
+    /**
+     * Open an XML file given a path.
+     *
+     * @param path to the file to open.
+     * @return a DOM object representing the data in the file.
+     */
     def openXML(path) {
         return DOMBuilder.parse(new StringReader(path.text), false, false).documentElement
     }
 
+    /**
+     * Create a new grade task.
+     */
     GradeTask() {
-        doLast {
-            // Load configurationdd
-            Yaml yaml = new Yaml();
-            def gradeConfiguration = yaml.load(project.file('config/grade/grade.yaml').text)
-            gradeConfiguration.students = yaml.load(project.file('config/grade/students.yaml').text)
-            gradeConfiguration.timestamp = System.currentTimeMillis()
+        /*
+         * Ensure that checkstyle doesn't fail the entire build, and always get rerun.
+         */
+        project.tasks.checkstyleMain.setIgnoreFailures(true)
+        project.tasks.checkstyleMain.outputs.upToDateWhen { false }
+    }
 
-            project.tasks.clean.execute()
+    /**
+     * Actually grade the assignment.
+     */
+    @TaskAction
+    def gradeAssignment() throws Exception {
 
+        Yaml yaml = new Yaml();
+        def gradeConfiguration = yaml.load(project.file('config/grade/grade.yaml').text)
+        gradeConfiguration.students = yaml.load(project.file('config/grade/students.yaml').text)
+        gradeConfiguration.timestamp = System.currentTimeMillis()
+
+        project.tasks.clean.execute()
+        project.tasks.checkstyleMain.execute()
+
+        /*
+         * We want to ignore errors in this block because we want to continue even if
+         * compiling or testing fails.
+         *
+         * Note that the idea of executing tests is usually a no-no in Gradle.
+         * But we have to do this if we want to give students partial credit.
+         */
+
+        def testOutputDirectories = []
+        gradeConfiguration.files.each{name ->
             try {
-                project.tasks.checkstyleMain.execute()
-            } catch (Exception e) {
-            }
-
-            def testOutputDirectories = []
-            gradeConfiguration.files.each{name ->
-                try {
-                    project.tasks.create(name: "compile" + name, type: JavaCompile) {
-                        source = project.sourceSets.main.java.srcDirs
-                        include name + ".java"
-                        classpath = project.sourceSets.main.compileClasspath
-                        destinationDir = project.sourceSets.main.output.classesDir
-                    }.execute()
-                } catch (Exception e) { }
-                try {
-                    project.tasks.create(name: "compileTest" + name, type: JavaCompile) {
-                        source = project.sourceSets.test.java.srcDirs
-                        include name + "Test.java"
-                        classpath = project.sourceSets.test.compileClasspath
-                        destinationDir = project.sourceSets.test.output.classesDir
-                    }.execute()
-                } catch (Exception e) { }
-                try {
-                    def test = project.tasks.create(name: "test" + name, type: Test, dependsOn: 'compileTest' + name) {
-                        useTestNG() {
-                            useDefaultListeners = true
-                        }
-                        reports.html.enabled = false
-                        include "**" + name + "Test**"
-                    }
-                    testOutputDirectories.add(test.reports.getJunitXml().getDestination())
-                    test.execute()
-                } catch (Exception e) { }
-            }
-
-            // Investigate checkstyle results
-            def toKeep = []
-            if (gradeConfiguration.containsKey('checkstyle')) {
-                def checkstyleResultsPath = project.tasks.checkstyleMain.getReports().getXml().getDestination()
-                try {
-                    def checkstyleResults = openXML(checkstyleResultsPath)
-                    gradeConfiguration.checkstyle.remove('missing')
-                    use (DOMCategory) {
-                        gradeConfiguration.checkstyle.selectors.each { checkstyleSelector ->
-                            if (checkstyleResults.xpath(checkstyleSelector.selector, BOOLEAN)) {
-                                toKeep.add(checkstyleSelector)
-                            }
-                        }
-                    }
-                    def toRemove = gradeConfiguration.checkstyle.selectors - toKeep
-                    toRemove.each { checkstyleSelector ->
-                        gradeConfiguration.checkstyle.selectors.remove(checkstyleSelector)
-                    }
-                } catch (Exception e) {
-                    println e
-                    gradeConfiguration.checkstyle.selectors = [ gradeConfiguration.checkstyle.missing ]
-                }
-            }
-
-            def mergedXML = new XmlSlurper().parseText("<testsuites></testsuites>")
-
-            // Investigate TestNG results
-            def testResultsDirectory = project.tasks.test.reports.getJunitXml().getDestination()
-            toKeep = []
-            testOutputDirectories.each{ testOutputDirectory ->
-                if (testOutputDirectory.exists()) {
-                    testOutputDirectory.eachFileMatch(~/.*\.xml/) { testResultsPath ->
-                        def testResults = new XmlSlurper().parse(testResultsPath)
-                        mergedXML.appendNode(testResults)
-                    }
-                }
-            }
-            mergedXML = DOMBuilder.parse(new StringReader(groovy.xml.XmlUtil.serialize(mergedXML)), false, false).documentElement
-            use (DOMCategory) {
-                gradeConfiguration.test.selectors.each { testSelector ->
-                    if (mergedXML.xpath(testSelector.selector, BOOLEAN)) {
-                        toKeep.add(testSelector)
-                    }
-                }
-            }
-
-            def toRemove = gradeConfiguration.test.selectors - toKeep
-            toRemove.each { testSelector ->
-                gradeConfiguration.test.selectors.remove(testSelector)
-            }
-
-            def totalScore = 0
-            println ""
-            println "".padRight(78, "-")
-            println gradeConfiguration.name + " Grade Summary"
-            println "".padRight(78, "-")
-            gradeConfiguration.each{ unused, v ->
-                if (v instanceof Map && v.containsKey('selectors')) {
-                    v.selectors.each{ selector ->
-                        print selector.name.padRight(20)
-                        print selector.score.toString().padLeft(8)
-                        print "     " + selector.message
-                        print '\n'
-                        totalScore += selector.score
-                    }
-                }
-            }
-            println "".padRight(78, "-")
-            print "Total Points".padRight(20)
-            print totalScore.toString().padLeft(8)
-            print '\n'
-            println "".padRight(78, "-")
-
-            gradeConfiguration.totalScore = totalScore
-
-            def gradePost = new HttpPost(gradeConfiguration.post)
-            gradePost.addHeader("content-type", "application/json")
-            gradePost.setEntity(new StringEntity(JsonOutput.toJson(gradeConfiguration)))
-
-            def client = HttpClientBuilder.create().build()
+                project.tasks.create(name: "compile" + name, type: JavaCompile) {
+                    source = project.sourceSets.main.java.srcDirs
+                    include name + ".java"
+                    classpath = project.sourceSets.main.compileClasspath
+                    destinationDir = project.sourceSets.main.output.classesDir
+                }.execute()
+            } catch (Exception e) { }
             try {
-                def response = client.execute(gradePost)
+                project.tasks.create(name: "compileTest" + name, type: JavaCompile) {
+                    source = project.sourceSets.test.java.srcDirs
+                    include name + "Test.java"
+                    classpath = project.sourceSets.test.compileClasspath
+                    destinationDir = project.sourceSets.test.output.classesDir
+                }.execute()
+            } catch (Exception e) { }
+            try {
+                def test = project.tasks.create(name: "test" + name, type: Test, dependsOn: 'compileTest' + name) {
+                    useTestNG() { useDefaultListeners = true }
+                    reports.html.enabled = false
+                    include "**" + name + "Test**"
+                }
+                testOutputDirectories.add(test.reports.getJunitXml().getDestination())
+                test.execute()
             } catch (Exception e) { }
         }
+
+        // Investigate checkstyle results
+        def toKeep = []
+        if (gradeConfiguration.containsKey('checkstyle')) {
+            def checkstyleResultsPath = project.tasks.checkstyleMain.getReports().getXml().getDestination()
+            try {
+                def checkstyleResults = openXML(checkstyleResultsPath)
+                gradeConfiguration.checkstyle.remove('missing')
+                use (DOMCategory) {
+                    gradeConfiguration.checkstyle.selectors.each { checkstyleSelector ->
+                        if (checkstyleResults.xpath(checkstyleSelector.selector, BOOLEAN)) {
+                            toKeep.add(checkstyleSelector)
+                        }
+                    }
+                }
+                def toRemove = gradeConfiguration.checkstyle.selectors - toKeep
+                toRemove.each { checkstyleSelector ->
+                    gradeConfiguration.checkstyle.selectors.remove(checkstyleSelector)
+                }
+            } catch (Exception e) {
+                println e
+                gradeConfiguration.checkstyle.selectors = [
+                    gradeConfiguration.checkstyle.missing
+                ]
+            }
+        }
+
+        def mergedXML = new XmlSlurper().parseText("<testsuites></testsuites>")
+
+        // Investigate TestNG results
+        def testResultsDirectory = project.tasks.test.reports.getJunitXml().getDestination()
+        toKeep = []
+        testOutputDirectories.each{ testOutputDirectory ->
+            if (testOutputDirectory.exists()) {
+                testOutputDirectory.eachFileMatch(~/.*\.xml/) { testResultsPath ->
+                    def testResults = new XmlSlurper().parse(testResultsPath)
+                    mergedXML.appendNode(testResults)
+                }
+            }
+        }
+        mergedXML = DOMBuilder.parse(new StringReader(groovy.xml.XmlUtil.serialize(mergedXML)), false, false).documentElement
+        use (DOMCategory) {
+            gradeConfiguration.test.selectors.each { testSelector ->
+                if (mergedXML.xpath(testSelector.selector, BOOLEAN)) {
+                    toKeep.add(testSelector)
+                }
+            }
+        }
+
+        def toRemove = gradeConfiguration.test.selectors - toKeep
+        toRemove.each { testSelector ->
+            gradeConfiguration.test.selectors.remove(testSelector)
+        }
+
+        def totalScore = 0
+        println ""
+        println "".padRight(78, "-")
+        println gradeConfiguration.name + " Grade Summary"
+        println "".padRight(78, "-")
+        gradeConfiguration.each{ unused, v ->
+            if (v instanceof Map && v.containsKey('selectors')) {
+                v.selectors.each{ selector ->
+                    print selector.name.padRight(20)
+                    print selector.score.toString().padLeft(8)
+                    print "     " + selector.message
+                    print '\n'
+                    totalScore += selector.score
+                }
+            }
+        }
+        println "".padRight(78, "-")
+        print "Total Points".padRight(20)
+        print totalScore.toString().padLeft(8)
+        print '\n'
+        println "".padRight(78, "-")
+
+        gradeConfiguration.totalScore = totalScore
+
+        def gradePost = new HttpPost(gradeConfiguration.post)
+        gradePost.addHeader("content-type", "application/json")
+        gradePost.setEntity(new StringEntity(JsonOutput.toJson(gradeConfiguration)))
+
+        def client = HttpClientBuilder.create().build()
+        try {
+            def response = client.execute(gradePost)
+        } catch (Exception e) { }
     }
 }
-
-// vim: ts=4:sw=4:et:ft=groovy
