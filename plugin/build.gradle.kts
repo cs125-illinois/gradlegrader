3f5821012458ac5112a77436558ed10477c55159
Geoffrey Challen <challen@illinois.edu>
2021-02-17 15:34:18 -0600

    Depedency updates for Spring 2021.

--- plugin/build.gradle.kts
+++ plugin/build.gradle.kts
@@ -11,14 +11,15 @@ version = "2021.2.0"
 
 dependencies {
     implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
     implementation(gradleApi())
     implementation("com.google.code.gson:gson:2.8.6")
     implementation("org.apache.httpcomponents:httpclient:4.5.13")
     implementation("org.apache.commons:commons-text:1.9")
     implementation("org.eclipse.jgit:org.eclipse.jgit:5.10.0.202012080955-r")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.12.1")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.12.1")
-    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.12.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.11.3")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.11.3")
+    implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.11.3")
 }
 gradlePlugin {
     plugins {
